476684f1216913f44f14f3aaf9736db208c5a29e
Bo Lorentsen <bl@lue.dk>
2017-08-15 14:02:08 +0200

    index exporting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // define how the module will look like
 export {locale_cb_set, header_add} from './common'
 export {rpc,fetch_smd} from './rpc'
-export {Entity, Iter, WatchCallback} from './store'
+export {Entity, Iter, WatchCallback, UpdateEntity} from './store'
 export {RestIter, RestEntity} from './restful'
